27ed34a6b12bd298f97f24603d84fbf1d2da7ea3
Hector <hr9457@gmail.com>
2020-08-20 09:24:43 -0600

    metodos para para distigir caracter

--- parseHTML/AnalizadorHTML.py
+++ parseHTML/AnalizadorHTML.py
@@ -10,11 +10,12 @@ class AnalizadorHTML:
         self.fila = 0        
         self.columna = 0        
         self.caracter = ''
-        self.listadoEtiquetas = ["<html>","</html>","<head>","</head>","<body>","</body>"
-        ,"<h1>","</h1>","<h2>","</h2>","<h3>","</h3>","<h4>","</h4>","<h5>","</h5>","<h6>","</h6>"
-        ,"<p>","</p>","<ul>","</ul>","<li>","</li>","<tr>","</tr>","<td>","</td>","<caption>","</caption>"
-        ,"<colgroup>","</colgroup>","<col>","</col>","<thead>","</thead>","<tbody>","</tbody>"
-        ,"<tfoot>","</tfoot>"]
+        self.listadoEtiquetas = ["html","/html","head","/head","body","/body"
+        ,"h1","/h1>","h2","/h2","h3","/h3","h4","/h4","h5","/h5","h6","/h6"
+        ,"p","/p","ul","/ul","li","/li","tr","/tr","td","/td","caption","/caption"
+        ,"colgroup","/colgroup","col","/col","thead","/thead","tbody","/tbody"
+        ,"tfoot","/tfoot"]
+        self.listadoAtributos = ["src","href","style","border"]
         self.listadoErrores = []
 
 
@@ -28,74 +29,27 @@ class AnalizadorHTML:
         # leng da el tamanio del texto
         while indice < len(self.txtEntrada):  
 
-            # estasdo 0 de simbolos
-            # simbolos de etiquetas principales < / > 
-            if self.estado == 0: 
-
-                # revision para aceptacion de una etiqueta
-                if (self.txtEntrada[puntero] == chr(60)
-                and self.txtEntrada[puntero + 1] != ' ' ):
-
-                    # concateno para la verificacion de la etiquetas
-                    self.token = self.token + self.txtEntrada[puntero]
-                    puntero += 1
-                    self.estado = 1
-
-                elif (self.txtEntrada[puntero] == chr(60) 
-                and self.txtEntrada[puntero] == ' ' ):
-                    # paso al estado 3
-                    self.estado = 3
-
-                
-                # verificiando la etiquetas de cierre
-                elif self.txtEntrada[puntero] == chr(62):
-                    
-                    # concateno
-                    self.token = self.token + self.txtEntrada[puntero]
-                    
-                    # busco si es etiqueta valida
-                    if self.token in self.listadoEtiquetas:
-                        pass
-                    else:
-                        self.listadoErrores.append([self.numeroError,self.fila,self.columna,self.token])
-
-                    # paso al siguiente caracter
-                    self.token = ""
-                    puntero += 1
-                    self.estado = 0
+            pass
 
 
-                # simbolo de = 
-                elif self.txtEntrada[puntero] == chr(61):
-                    # concateno
-                    self.token = self.token + self.txtEntrada[puntero]
-                    puntero += 1
-                    self.estado = 0
+    def letter(self,caracter):
+        if ( (caracter >= chr(65) 
+        and caracter <= chr(90))
+        or caracter >= chr(97)
+        and caracter <= chr(122)):
+            return True
 
-                # simbolo de "
-                elif self.txtEntrada[puntero] == chr(34):
-                    #concateno
-                    self.token = self.token + self.txtEntrada[puntero]
-                    puntero += 1
-                    self.estado = 0
-                
-                # moviento en el cursos 
-                elif (self.txtEntrada[puntero] == ' ' 
-                or self.txtEntrada[puntero] == '\n' 
-                or self.txtEntrada[puntero] == '\t'):
-
-                    # lo ignoro
-                    puntero += 1
+        else: 
+            return False
 
 
-            # estado para letras 
-            elif self.estado == 1:
-                pass
+    def Digit(self,caracter):
+        if (caracter >= chr(48)
+        and caracter <= chr(57)):
+            return True
 
-
-            # tercer estado
-            elif self.estado == 3:
-                pass
+        else:
+            return False
 
 
 
